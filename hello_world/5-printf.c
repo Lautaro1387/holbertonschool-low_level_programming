4a3952dd32ec529fa3b4d4309130832f0f0c10da
lautaroilla <5134@holbertonschool.com>
2022-06-06 07:25:48 -0700

    Task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - This is a description
- *   * Return: hola
- *    */
+ * main - This is a description
+ * Return: hola
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
