f16cb6310a15218bd8d286fa16b803d83d8640b5
lautaroilla <5134@holbertonschool.com>
2022-06-06 10:32:49 -0700

    Task 8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-fwrite(int 2,char * "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(int 2,char * "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
