cec918da54ccd8d3c7da0ec2c8c7990d1d3ae6d6
lautaroilla <5134@holbertonschool.com>
2022-06-06 06:53:09 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
-int puts(void){
-echo "Programming is like building a multilingual puzzle";
+int main(void){
+ puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
