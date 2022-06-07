84237f6d54fa200bc95e77fc0c4d9093d8efb932
lautaroilla <5134@holbertonschool.com>
2022-06-07 16:24:35 -0700

    Task 11

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -6,29 +6,29 @@
 int main(void)
 {
 int n;
-int o;
+int e;
 int m;
 int a = 0;
 int b = 0;
-for (n = 48; n <= 55; n++)
+for (n = 48 ; n <= 55 ; n++)
 {
-for (o = 49 + a; o <= 56; o++)
+for (e = 49 + a ; e <= 56 ; e++)
 {
-for (m = 50 + b; m <= 57; m++)
+for (m = 50 + b ; m <= 57; m++)
 {
-putchar(n);
-putchar(o);
-putchar(m);
+putchar (n);
+putchar (e);
+putchar (m);
 if (n < 55)
 {
-putchar(44);
-putchar(32);
+putchar (44);
+putchar (32);
 }
 }
 b++;
 }
 a++;
 }
-putchar(10);
+putchar (10);
 return (0);
 }
