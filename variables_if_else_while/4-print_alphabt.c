dd8d31d47af86d4af4650b7f03c8557a90810514
lautaroilla <5134@holbertonschool.com>
2022-06-07 08:24:10 -0700

    Task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Alphabet except p and e
+ * Return: Except q and e
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch = !q && ch = !e; c++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
