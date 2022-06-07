209c013ad0da2f3711ee6443ee3b175de12ff2d9
lautaroilla <5134@holbertonschool.com>
2022-06-07 15:39:10 -0700

    Task 10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,20 @@
 int main(void)
 {
 int n;
+int o = 49;
+int m;
 for (n = 48; n <= 57; n++)
+{
+for (m = 49; m <= 57; m++)
 {
 putchar(n);
 if (n < 57)
 {
-putchar(',');
-putchar(' ');
-}
-else if (n > 0)
-{
-putchar(n);
+putchar(44);
+putchar(32);
 }
 }
-putchar('\n');
+}
+putchar(10);
 return (0);
 }
