640db0340f58cf802a6df2e1583f780a23090ce9
lautaroilla <5134@holbertonschool.com>
2022-06-07 05:35:06 -0700

    Task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf ("is positive");
+printf (n,"is positive\n");
 }
 if (n == 0)
 {
-printf("is zero");
+printf(n,"is zero\n");
 }
 if (n < 0) 
 {
-printf ("is negative");
+printf (n,"is negative\n");
 }
 return (0);
 }
