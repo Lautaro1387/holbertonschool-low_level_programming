b92e61497681efdf19cdf0efcec70c9e62b45ba0
lautaroilla <5134@holbertonschool.com>
2022-06-07 15:53:21 -0700

    Task 11

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Number
+ * Return: finished
+ */
+int main(void)
+{
+int n;
+int o = 49;
+int m;
+int p = 50;
+int l;
+for (n = 48; n <= 57; n++)
+{
+for (m = o; m <= 57; m++)
+{
+for (l = p; l <= 57; l++)
+{
+putchar(n);
+putchar(m);
+if (n < 56)
+{
+putchar(44);
+putchar(32);
+}
+}
+o++;
+}
+p++
+}
+putchar(10);
+return (0);
+}
