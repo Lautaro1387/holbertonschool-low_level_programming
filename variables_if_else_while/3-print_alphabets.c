78850ead66b58e327f13799d4a53873bd714d357
lautaroilla <5134@holbertonschool.com>
2022-06-07 07:27:05 -0700

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Alphabet 
+ * Return: finished
+ */
+int main(void)
+{
+char ch
+for (ch = 'a'; ch <= 'z'; ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n')
+return (0);
+}
