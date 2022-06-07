1ec1383ca10bdbb0df853d27aa7f2b8a0e983fa6
lautaroilla <5134@holbertonschool.com>
2022-06-07 09:10:29 -0700

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Alphabet except p and e
+ * Return: Except q and e
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
