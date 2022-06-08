2df382a8f3f4af1a4c276bd98f746c94dd9ddfe1
lautaroilla <5134@holbertonschool.com>
2022-06-07 17:30:50 -0700

    Task 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Number
+ * Return: finished
+ */
+int main(void)
+{
+	int n;
+	int e;
+	for (n = 0; n < 100; n++)
+	{
+		for (e = 0; e < 100; e++)
+		{
+				if (n < e)
+				{
+					putchar ((n) + 48);
+					putchar ((n) + 48);
+					putchar(32);
+					putchar ((e) + 48);
+					putchar ((e) + 48);
+					if (n != 98 || e != 99)
+					{
+						putchar (44);
+						putchar (32);
+					}
+				}
+			}
+		}
+	}
+	putchar (10);
+	return (0);
+}
