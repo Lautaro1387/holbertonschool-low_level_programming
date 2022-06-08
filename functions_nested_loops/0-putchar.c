f22835e170c29cb7df02e5e5383dab20e99f0f62
lautaroilla <5134@holbertonschool.com>
2022-06-08 07:10:48 -0700

    Task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
-#include "main.h"
+#include <main.h>
 /**
  * main - Write a program that prints _putchar
  * Return: return...
  */
-int _putchar(char p)
+int main(void)
 {
-	char p = "_putchar";
-	putchar(p);
+	
 	putchar(10);
 	return (0);
 }
