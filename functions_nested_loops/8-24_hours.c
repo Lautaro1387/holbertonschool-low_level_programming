2e88b88691f9992f6ebe8ab98bb6256c6d579d4b
lautaroilla <5134@holbertonschool.com>
2022-06-09 11:09:10 -0700

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,18 +8,18 @@
 void jack_bauer(void)
 {
 	int min1, min2, hors1, hors2;
-	for (min1 = '0'; min1 <= '2')
+	for (min1 = '0'; min1 <= '2');
 	{
-	for (hors1 = '0'; hors1 <= '3')
+	for (hors1 = '0'; hors1 <= '3');
 	{
 	_putchar(min1);
 	_putchar(hors1);
 	_putchar(58);
 	}
 	}
-	for (min2 = '0'; hors2 <= '5')
+	for (min2 = '0'; hors2 <= '5');
 	{
-	for (min2 = '0'; hors2 <= '9')
+	for (min2 = '0'; hors2 <= '9');
 	{
 	_putchar(min2);
 	_putchar(hors2);
