088025c1911b1de20684927b5a154c2e04da9e74
lautaroilla <5134@holbertonschool.com>
2022-06-09 05:03:13 -0700

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Write a function that prints the alphabet, in lowercase.
+ * Return: Alphabet.
+ */
+void print_alphabet(void)
+{
+	int x;
+	for(x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar(10);
+	return(0);
+}
