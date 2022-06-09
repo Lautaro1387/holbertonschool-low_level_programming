823d9a14a612be3716dd7f1328a71ee04f7a567d
lautaroilla <5134@holbertonschool.com>
2022-06-09 07:14:19 -0700

    Task 2

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - Checks for lowercase character.
+ * Return: 1 if c and 0 otherwise.
+ */
+int _islower(int c)
+{
+	int c;
+
+	for (c = 'c' ; c <= 'c'; c++)
+	{
+	if (c = 'c')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	}
+}
