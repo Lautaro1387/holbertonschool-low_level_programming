4cc034263123cb5b76cd6635d942f47210f2b203
lautaroilla <5134@holbertonschool.com>
2022-06-09 09:44:36 -0700

    Task 5

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * Program that prints _putchar, followed by a new line.
+ */
 int _putchar(char);
+/**
+ * Function that prints the alphabet, in lowercase, followed by a new line.
+ */
 void print_alphabet(void);
+/**
+ * Function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ */
 void print_alphabet_x10(void);
+/**
+ * Function that checks for lowercase character.
+ */
 int _islower(int c);
+/**
+ * Function that checks for alphabetic character.
+ */
 int _isalpha(int c);
+/**
+ * Function that prints the sign of a number.
+ */
+int print_sign(int n);
 #endif
