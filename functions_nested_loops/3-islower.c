408d9d7a64268c3154f10be911f9d924ef625b7f
lautaroilla <5134@holbertonschool.com>
2022-06-09 07:20:16 -0700

    Task 2

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,22 +2,21 @@
 #include <ctype.h>
 
 /**
- * _islower - Checks for lowercase character.
+ * _islower.c - Checks for lowercase character.
  * Return: 1 if c and 0 otherwise.
  */
 int _islower(int c)
 {
-	int c;
+int c = 'c';
 
-	for (c = 'c' ; c <= 'c'; c++)
-	{
-	if (c = 'c')
-	{
-	return (1);
-	}
-	else
-	{
-	return (0);
-	}
-	}
+
+if ( islower(c) )
+{
+return (1);
+}
+elseif ( islower(c) )
+{
+return (0);
+}
+}
 }
