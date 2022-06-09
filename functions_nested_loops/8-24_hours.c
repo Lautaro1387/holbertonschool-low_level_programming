7be7224574689b82393e2ec6df218d9b448ac5d3
lautaroilla <5134@holbertonschool.com>
2022-06-09 11:08:37 -0700

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,24 +2,24 @@
 #include <ctype.h>
 #include <stdlib.h>
 /**
- * print_last_digit - Prints the sign of a number.
+ * jacl_bauer - Prints the sign of a number.
  * Return: Anything.
  */
 void jack_bauer(void)
 {
 	int min1, min2, hors1, hors2;
-	for (min = '0'; hors <= '2')
+	for (min1 = '0'; min1 <= '2')
 	{
-	for (hors = '0'; hors <= '3')
+	for (hors1 = '0'; hors1 <= '3')
 	{
 	_putchar(min1);
 	_putchar(hors1);
 	_putchar(58);
 	}
 	}
-	for (hors = '0'; hors <= '5')
+	for (min2 = '0'; hors2 <= '5')
 	{
-	for (hors = '0'; hors <= '9')
+	for (min2 = '0'; hors2 <= '9')
 	{
 	_putchar(min2);
 	_putchar(hors2);
