34b1848b48354907b3d7a94b7834fcc3b4f708f3
lautaroilla <5134@holbertonschool.com>
2022-06-09 10:05:35 -0700

    Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * _abs - Prints the sign of a number.
+ * @n: The check.
+ * Return: Anything.
+ */
+int _abs(int n)
+{
+int n = abs(int n);
+
+_putchar(n);
+return (0);
+}
