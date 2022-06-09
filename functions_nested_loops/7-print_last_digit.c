44c575778166f3d6e2e9599f43dd4e78a0473e2b
lautaroilla <5134@holbertonschool.com>
2022-06-09 10:37:40 -0700

    Task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,13 @@
 #include <ctype.h>
 #include <stdlib.h>
 /**
- * print_last_digit.c - Prints the sign of a number.
+ * print_last_digit - Prints the sign of a number.
  * @n: The check.
  * Return: Anything.
  */
 int print_last_digit(int n)
 {
 int nf = n % 10;
-
+_putchar(nf + n);
 return (nf);
 }
