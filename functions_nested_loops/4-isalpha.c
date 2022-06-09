d00e1805cd0bf5bb3305dc76ec0f4ea2ae48b4cb
lautaroilla <5134@holbertonschool.com>
2022-06-09 09:10:18 -0700

    Task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - Checks for alphabetic character.
+ * Return: return 1 if c is a let,low,upper else return 0
+ */
+int _isalpha(int c)
+{
+	if (c = 'c'; c <= 'C')
+	{
+	else if (c = 'A'; c <= 'Z')
+	{
+		return (1);
+	}
+	}
+	else
+	{
+		return (0);
+	}
+
+}
