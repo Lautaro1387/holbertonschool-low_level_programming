39578d230f0188bfc8622d5655621cd10e49ac1a
lautaroilla <5134@holbertonschool.com>
2022-06-09 06:23:12 -0700

    Task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet.
+ * Return: 10 times.
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		if(alphabet = 'a')
+		{
+			return (alphabet);
+		}
+		_putchar(alphabet);
+	}
+_putchar(10);
+}
