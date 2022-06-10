ff5943b57d19733ef0bd8b926a1cff453267c445
lautaroilla <5134@holbertonschool.com>
2022-06-10 08:36:41 -0700

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * print_to_98 - Prints the sign of a number.
  * @n: Check.
@@ -8,11 +9,11 @@
  */
 void print_to_98(int n)
 {
-	int a = 48;
 
 for (n = a; n <= 98; n++)
 {
 _putchar(n);
+printf("%d\n", n)
 _putchar(',');
 _putchar(32);
 }
