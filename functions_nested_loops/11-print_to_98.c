54dcd6f86ac4f709c5edf7f900638c8cc6b6ed13
lautaroilla <5134@holbertonschool.com>
2022-06-10 09:30:26 -0700

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,15 @@ if (n <= 98)
 for (n = n; n <= 98; n++)
 {
 printf("%d, ", n);
+printf("%d\n", 98)
 }
-}
-if(n > 0)
+else
 {
 for (n = n; n >= 98; n--)
 {
-printf("-%d, ", n);
+printf("%d, ", n);
+printf("%d\n", 98);
 }
 }
-_putchar('\n');
+}
 }
