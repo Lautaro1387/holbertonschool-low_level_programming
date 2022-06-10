2558dbee300ac78736fe00eeae8d24edfa25f7b1
lautaroilla <5134@holbertonschool.com>
2022-06-10 05:13:06 -0700

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,16 +9,12 @@ void jack_bauer(void)
 {
 	int min1, min2, hors1, hors2;
 
-	for (hors1 = 48; hors1 <= 57; hors1++)
+	for (hors1 = 48; hors1 <= 50; hors1++)
 	{
-	for (hors2 = 48; hors2 <= 57; hors2++)
+	for (hors2 = 48; hors2 <= 51; hors2++)
 	{
 	_putchar(hors1);
 	_putchar(hors2);
-	if (hors1 >= 48)
-	{
-		_putchar(58);
-	}
 	if (hors2 >= 48)
 	{
 		_putchar(58);
