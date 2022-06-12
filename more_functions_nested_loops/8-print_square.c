73b765bf7c039faaa6a5eb28d04a9715372b367b
lautaroilla <5134@holbertonschool.com>
2022-06-12 13:01:03 -0700

    Task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,7 +5,7 @@
  */
 void print_square(int size)
 {
-int a;
+int a, b;
 
 if (size > 0)
 {
@@ -13,6 +13,7 @@ _putchar(35);
 _putchar(10);
 for (a = 1; a <= size; a++)
 {
+for (b = 1; b >= a; b++)
 _putchar(35);
 _putchar(10);
 }
