8193a73dff332cca5b2ff38f272538cf4b2b3ed0
lautaroilla <5134@holbertonschool.com>
2022-06-12 09:03:59 -0700

    Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - Function that prints 10 times the numbers, from 0 to 14.
+ */
+void more_numbers(void)
+{
+int times;
+int numbers;
+for (times = '0'; times <= '0'; times++)
+{
+for (numbers = 0; numbers <= 14; numbers++)
+{
+_putchar(times);
+_putchar(numbers);
+}
+}
+_putchar(10);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -24,4 +24,8 @@ void print_numbers(void);
  * Function that prints the numbers, from 0 to 9.
  */
 void print_most_numbers(void);
+/**
+ * Function that prints 10 times the numbers, from 0 to 14.
+ */
+void more_numbers(void);
 #endif
