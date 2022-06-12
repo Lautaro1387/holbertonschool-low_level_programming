b0755a4fe7525a474987fa9914f200cc583e1873
lautaroilla <5134@holbertonschool.com>
2022-06-12 15:37:43 -0700

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,28 +3,28 @@
 /**
  * 
  */
-void Fizz_buzz(int test)
+void main(void)
 {
+int test;
+
 for (test = 0; test <= 100; test++)
 {
-	if (test % 3)
+	if (test % 3 == 0)
 	{
 		printf("Fizz");
-		_putchar(32);
 	}
 	else if (test % 5 == 0)
 	{
 		printf("Buzz");
-		_putchar(32);
 	}
 	else if (test % 15 == 0)
 	{
 		printf("FizzBuzz");
-		_putchar(32);
 	}
 	if (test != 100)
 	{
 		printf(" ");
 	}
 }
+printf("\n";)
 }
