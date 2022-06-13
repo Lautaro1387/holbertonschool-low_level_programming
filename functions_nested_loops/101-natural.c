04cf688e3b18363c899fb93880b49e6419ab00ff
lautaroilla <5134@holbertonschool.com>
2022-06-12 19:47:17 -0700

    Task 12

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - Print multiples of 3 or 5.
+ * Return: by a new line.
+ */
+int main(void)
+{
+long int sum, sum3, sum5;
+int i;
+
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("\n", sum);
+	return (0);
+}
+}
