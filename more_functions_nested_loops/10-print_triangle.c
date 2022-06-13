f4e2819089c362c47cf5683c67be2d1aa655e9dc
lautaroilla <5134@holbertonschool.com>
2022-06-12 19:26:28 -0700

    Task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,27 +5,26 @@
  */
 void print_triangle(int size)
 {
-int a, b, c, d;
-
-d = 1
-	if (size <= 0)
-	{
-	_putchar(10);
-	}
-	else if (size > 0)
-	{
-		for (a = 1; a <= size; a++)
-		{
-		for (b = 1; b <= (size - d); b++)
-		{
-		_putchar(' ');
-		}
-		for (c = 1; c <= a; c++)
-		{
-		_putchar(35);
-		}
-		d = d + 1;
-		_putchar(10);
-	}
-	}
+int a, b, c, x;
+x = 1;
+if (size <= 0)
+{
+_putchar(10);
+}
+else if (size > 0)
+{
+for (a = 1; a <= size; a++)
+{
+for (b = 1; b <= (size - x); b++)
+{
+_putchar(' ');
+}
+for (c = 1; c <= a; c++)
+{
+_putchar(35);
+}
+x = x + 1;
+_putchar(10);
+}
+}
 }
