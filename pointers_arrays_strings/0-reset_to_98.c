645e5ca1aae3496eca624d019d20029758178db5
lautaroilla <5134@holbertonschool.com>
2022-06-14 08:45:05 -0700

    Task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 -Function that takes a pointer to an int as parameter.
+ */
+void reset_to_98(int *n)
+{
+	int c = (*n[98]);
+
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * Function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ */
+void reset_to_98(int *n);
+#endif
