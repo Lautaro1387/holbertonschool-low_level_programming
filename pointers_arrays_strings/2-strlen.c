b3a1007e4af57d91804a17b76ff809fb667c95f0
lautaroilla <5134@holbertonschool.com>
2022-06-14 09:20:45 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - Function that returns the length of a string.
+ * @s: char.
+ */
+int _strlen(char *s)
+{
+	strlen(*s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,4 +8,8 @@ void reset_to_98(int *n);
  * Function that swaps the values of two integers.
  */
 void swap_int(int *a, int *b);
+/**
+ * Function that returns the length of a string.
+ */
+int _strlen(char *s);
 #endif
