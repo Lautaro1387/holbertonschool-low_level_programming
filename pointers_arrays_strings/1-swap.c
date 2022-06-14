fb69df277978e476f10e048d90928c3e64010667
lautaroilla <5134@holbertonschool.com>
2022-06-14 09:02:07 -0700

    Task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Function that swaps the values of two integers.
+ * @a: int.
+ * @b: int.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,4 +4,8 @@
  * Function that takes a pointer to an int as parameter and updates the value it points to to 98.
  */
 void reset_to_98(int *n);
+/**
+ * Function that swaps the values of two integers.
+ */
+void swap_int(int *a, int *b);
 #endif
