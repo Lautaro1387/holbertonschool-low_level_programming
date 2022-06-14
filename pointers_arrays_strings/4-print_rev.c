0b86348c9ea5c82332b1e3c83d261c8ac02cb34f
lautaroilla <5134@holbertonschool.com>
2022-06-14 10:02:11 -0700

    Task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints a string.
+ * @s: PRINTS
+ */
+void print_rev(char *s)
+{
+	printf(rev("%s\n", s));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,4 +16,8 @@ int _strlen(char *s);
  * Function that prints a string, followed by a new line, to stdout.
  */
 void _puts(char *str);
+/**
+ * Function that prints a string, in reverse, followed by a new line.
+ */
+void print_rev(char *s);
 #endif
