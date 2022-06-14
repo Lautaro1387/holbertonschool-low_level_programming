d68ac39555ec877b68783f7b03f2127732c39d05
lautaroilla <5134@holbertonschool.com>
2022-06-14 11:05:45 -0700

    Task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,8 +7,14 @@
  */
 void print_rev(char *s)
 {
-	char c = *s;
+	char c;
 
-	c = strrev(s);
-	printf("%s\n", c);
+	for (c = "0"; *s != 0; c++)
+	{
+	for (c = "0"; *s != 0; c--)
+	{
+	_putchar(c);
+	}
+	}
+	_putchar(10);
 }
