562333399087db204055004d3bcce5bd2d8fc284
lautaroilla <5134@holbertonschool.com>
2022-06-15 08:12:08 -0700

    Task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 /**
  * rev_string - Function that reverses a string.
+ * @s: Character.
  */
 void rev_string(char *s)
 {
-	int a, b;
+	int a, b, c;
 
-	a = strlen(s);
+	b = strlen(s);
 
-	for (b = a - 1; b >= 0; b--)
+	for (a = 0; a < b/2; a++)
 	{
-		_putchar(s[b]);
+		c = s[a];
+		s[a] = s[b - a - 1];
+		s[b - a - 1] = c;
+		_putchar(s[a]);
 	}
 	_putchar(10);
 }
