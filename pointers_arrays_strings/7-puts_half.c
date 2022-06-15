b0501cc8b4c00ce621075546f45d28df2d7390d2
lautaroilla <5134@holbertonschool.com>
2022-06-15 10:16:45 -0700

    Task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,12 @@ void puts_half(char *str)
 	int a, b;
 
 	b = strlen(str);
-	for (a = 0; a < b / 2; a += 0)
+	for (a = 0; a < b / 2; a++)
 	{
 		if (a % 2 == 0)
 		{
-		_putchar(str[a / 2]);
+		_putchar(str[a]);
 		}
 	}
+	_putchar(10);
 }
