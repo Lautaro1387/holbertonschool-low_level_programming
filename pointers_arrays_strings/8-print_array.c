0a3609ea6fc40811612f5a164a76045a71748d0f
lautaroilla <5134@holbertonschool.com>
2022-06-16 06:12:40 -0700

    Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_array - Function that prints n elements.
+ * @a, @b: elements.
+ */
+void print_array(int *a, int n)
+{
+	int n = strlen(a);
+
+	printf("%d", a[n]);
+	_putchar(10);
+}
