08457cc01a3564b80354316016b99972ff0395e4
lautaroilla <5134@holbertonschool.com>
2022-06-16 08:56:38 -0700

    Task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <cstring>
+/**
+ * strcpy - Function.
+ * @dest: Argument.
+ * @src: Argument.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return(dest);
