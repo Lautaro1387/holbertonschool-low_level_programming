95ddbcffe2501ad9d4de8d144ad92b6b3fba66c7
lautaroilla <5134@holbertonschool.com>
2022-06-16 10:35:12 -0700

    Task 0.1

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,11 @@ char *_strcat(char *dest, char *src)
 {
 	int a, b;
 
-	for (a = 0; src[a] != 0; a++)
+	for (a = 0; dest[a] != 0; a++)
 	{
-		dest[a] = src[a];
 	for (b = 0; src[b] != 0; b++)
 	{
+		dest[a] = src[a];
 		src[b] = dest[b];
 	}
 	}
