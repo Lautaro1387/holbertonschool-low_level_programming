9ce7c80fe10ea7aafb232264aba1004bc6cd1128
lautaroilla <5134@holbertonschool.com>
2022-06-16 16:38:33 -0700

    Task 2.0

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncpy - Function that copies a string.
+ * @dest: Element.
+ * @src: Element.
+ * @n: Element.
+ * Return: return.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a, b , c;
+
+	for (a = 0; dest[a] != 0; a++)
+	{
+	for (b = 0; src[b] !=0; b++)
+	{
+	c = dest[a];
+	dest[a] = src[b];
+	src[b] = c;
+	}
+	}
+	return (c);
+}
