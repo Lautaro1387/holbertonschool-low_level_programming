0d2618b9543284e2e1669589857cd0a072639105
lautaroilla <5134@holbertonschool.com>
2022-06-16 18:06:28 -0700

    Task 2.0

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a, b;
+	int a;
 
 	for (a = 0; a < n && dest[a] != 0; a++)
 	{
-	for (b = 0; b < n && src[b] != 0; b++)
+	for (a > 0; a < n && dest[a] != 0; a--)
 	{
 	dest[a] = src[b];
-	a++;
 	}
 	}
+	dest[a] = '\0';
 	return (dest);
 }
