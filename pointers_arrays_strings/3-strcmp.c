600c371d5571fa8239e02a72217aa3c998bb9793
lautaroilla <5134@holbertonschool.com>
2022-06-17 17:25:38 -0700

    Task 3.0

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int comp1;
+	while (*s1 == *s2)
+	{
+		return (0);
 
-	for (comp1 = 0; s1[comp1] != '\0'; comp1++)
-	{
-		s1[comp1] = s2[comp1];
+		if (*s1 > *s2)
+		{
+			return (+);
+		}
+		else if (*s1 < *s2)
+		{
+			return (-);
+		}
 	}
-	for (comp1 = 0; s2[comp1] != '\0'; comp1++)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 || *s2);
+	return (*s1 - *s2);
 }
