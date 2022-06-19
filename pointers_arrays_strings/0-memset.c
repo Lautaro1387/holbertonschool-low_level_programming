d6db9536572db8bf4ebff9ffeaa7efa23b289708
lautaroilla <5134@holbertonschool.com>
2022-06-19 15:40:12 -0700

    Task 0.3

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (n = 0; n[s] != '\0'; n++)
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
 	{
-		n[s] = b;
-		b = b[s];
-		b[s] = n[s];
+		s[a] = b;
 	}
 	return (s);
 }
