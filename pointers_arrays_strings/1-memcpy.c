62793362b51c484ceaca9e974f0b4fbcb909f22e
lautaroilla <5134@holbertonschool.com>
2022-06-19 16:08:19 -0700

    Task 1.3

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
-	char b;
 
 	for (a = 0; a < n; a++)
 	{
-		src[a] = b;
-		b = dest[a];
 		dest[a] = src[a];
 	}
 	return (dest);
