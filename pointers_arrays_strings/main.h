cb8e5f4f55dac4dd267b2014779cda070f00851e
lautaroilla <5134@holbertonschool.com>
2022-06-19 15:51:38 -0700

    Task 1.3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -67,4 +67,6 @@ char *cap_string(char *);
 char *leet(char *a);
 /** Function that fills memory with a constant byte.**/
 char *_memset(char *s, char b, unsigned int n);
+/** Function that copies memory area.**/
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
