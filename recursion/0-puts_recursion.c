cfba5780474ca0a02badce5332196bc1e2688ac7
lautaroilla <5134@holbertonschool.com>
2022-06-20 06:29:11 -0700

    Task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Function that prints a string.
+ * @s: Pointer element.
+ */
+void _puts_recursion(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		puts(s[a]);
+		_putchar(10);
+	}
+}
---
+++ recursion/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+/** Function that prints a string, followed by a new line. */
+void _puts_recursion(char *s);
+#endif
