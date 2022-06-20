3fe1c4d0538caed390b2c317743587198603be39
lautaroilla <5134@holbertonschool.com>
2022-06-20 09:59:24 -0700

    Task 3.0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,7 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(s);
-	_puts_recursion(s);
+	int a = 0;
+
+	if (s[a] != 0)
+	{
+	_putchar(s[a]);
+	_puts_recursion(s[a]);
+	}
+	else
 	_putchar(10);
 }
