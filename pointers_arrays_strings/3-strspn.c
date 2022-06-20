862cbdfb558a2b1c060a0d4c099c11944f495387
lautaroilla <5134@holbertonschool.com>
2022-06-19 17:47:58 -0700

    Task 2.3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,9 @@ unsigned int _strspn(char *s, char *accept)
 	int a, b, c, d;
 
 	c = 0;
-	d = 0;
 	for (a = 0; s[a] != '\0'; a++)
 	{
+	d = 0;
 	for (b = 0; accept[b] != '\0'; b++)
 	{
 		if (s[a] == accept[b])
