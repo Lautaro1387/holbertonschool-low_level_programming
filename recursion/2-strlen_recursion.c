bbdd934e42b9d9cd4e345ed3b83a394f3bb535f1
lautaroilla <5134@holbertonschool.com>
2022-06-20 10:28:48 -0700

    Task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -12,7 +12,8 @@ int _strlen_recursion(char *s)
 
 	if (*s != '\0')
 	{
+		_strlen_recursion(s[a]);
 		_putchar(s[a]);
-		return (1 + _strlen_recursion(s + 1));
 	}
+	return (1 + _strlen_recursion(s + 1));
 }
