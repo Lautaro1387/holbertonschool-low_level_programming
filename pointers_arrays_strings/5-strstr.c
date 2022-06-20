826127ba80fb008e6fdd2c313406451c00b6c971
lautaroilla <5134@holbertonschool.com>
2022-06-19 18:05:50 -0700

    Task 2.3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - Function that locates a substring.
+ * @haystack: Pointer element.
+ * @needle: Pointer element.
+ * Return: Pointer to the beginning.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+			if (haystack[a] == needle[b])
+			{
+			return (needle[a]);
+			}
+		}
+	}
+	return (NULL);
+}
