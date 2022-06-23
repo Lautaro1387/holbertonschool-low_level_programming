03729686e507cc280430b4f9428cdc3f979d95fc
lautaroilla <5134@holbertonschool.com>
2022-06-23 13:03:58 -0700

    Task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - Function that creates an array of chars.
+ * @size: Element.
+ * @c: Element.
+ * Return: NULL if size = 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	int a = argv(c);
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+	if (size != 0)
+	{
+	return (*c);
+	}
+	else
+	{
+	return (NULL);
+	}
+}
