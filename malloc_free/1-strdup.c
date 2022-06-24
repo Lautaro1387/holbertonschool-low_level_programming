d079003db7c045a525fbd2826a3d6af2a569f1d6
lautaroilla <5134@holbertonschool.com>
2022-06-24 13:44:05 -0700

    Task 4

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * _strdup - Function that creates an array of chars.
  * @str: Pointer element.
@@ -9,7 +8,6 @@
  */
 char *_strdup(char *str)
 {
-	char a = strlen(str);
 	char *c;
 	int b, d = 1;
 
