e5bf603525f6556b28df63947d841b57a6136fbd
lautaroilla <5134@holbertonschool.com>
2022-06-24 13:43:00 -0700

    Task 4

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,13 +11,13 @@ char *_strdup(char *str)
 {
 	char a = strlen(str);
 	char *c;
-	int b;
+	int b, d = 1;
 
 	if (str == NULL)
 	{
 	return (NULL);
 	}
-	c = malloc(a);
+	c = malloc(sizeof(char) * d + 1);
 	if (c == NULL)
 	{
 		return (NULL);
@@ -26,6 +26,5 @@ char *_strdup(char *str)
 	{
 		c[b] = str[b];
 	}
-	str[b] = '\0';
 	return (c);
 }
