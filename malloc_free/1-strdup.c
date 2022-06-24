d38604e9ab1b59e1e4816fed211cd03e30c33e9c
lautaroilla <5134@holbertonschool.com>
2022-06-24 10:52:03 -0700

    Task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - Function that creates an array of chars.
+ * @str: Pointer element.
+ * Return: NULL if insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	char *a = malloc(sizeof(*str));
+	unsigned int b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; str[b] != '\0'; b++)
+	{
+		str[b] = a;
+	}
+	return (NULL);
+}
