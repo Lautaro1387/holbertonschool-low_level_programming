7e5aac5ebec89d30e2931a6ab1d022eb298b6bf4
Lautaro1387 <5134@holbertonschool.com>
2022-06-25 14:59:34 -0700

    Task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * alloc_grid - Function that returns a pointer to a 2 dimensional.
+ * @width: Element.
+ * @height: Element.
+ * Return: Return two dimensional.
+ */
+int **alloc_grid(int width, int height)
+{
+	int *a;
+	int b = 0, c = 0;
+
+	while (b < width)
+	{
+	width++;
+	}
+	while (b < heigth)
+	{
+	height++;
+	}
+	a = malloc(width * height * sizeof(int));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	while (c < width && c < height)
+	{
+		a[c] = width[c];
+		a[c] = heigth[c];
+		c++;
+	}
+	if ((width[a] == 0 && height[a] == 0) || (width[a] < 0 && height[a] < 0))
+	{
+		return (NULL);
+	}
+	return (a);
+}
