097d05900ecb8d0b72a82de7b17fc3bf8ebce759
Lautaro1387 <5134@holbertonschool.com>
2022-06-25 20:02:22 -0700

    Task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - Function that frees a 2 dimensional.
+ * @grid: Pointer element.
+ * @height: Element.
+ */
+void free_grid(int **grid, int height)
+{
+	int b;
+
+	grid = malloc(sizeof(int) * alloc_grid);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; b < height; b++)
+	{
+		gird = malloc(sizeof(int) * height);
+		free(gird[b]);
+	}
+	return (gird);
+}
