647ef39161142def077eee3b111971865535f1b4
Lautaro1387 <5134@holbertonschool.com>
2022-06-28 15:50:43 -0700

    Task 0

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+include "dog.h"
+/**
+ * struct dog - Structure.
+ * @d: Pointer.
+ * @name: Pointer.
+ * @age: Element.
+ * @owner: Pointer.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	
+}
