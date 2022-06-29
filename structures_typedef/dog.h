a0efa83e246f1f37f80343a15f132e9b14ebb207
Lautaro1387 <5134@holbertonschool.com>
2022-06-28 19:04:39 -0700

    Task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,17 +14,9 @@ struct dog
 	char *owner;
 };
 /**
- * struct dog - Structure.
- * @name: name.
- * @age: age.
- * @owner: owner.
+ * dog_t - Structure.
  */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
