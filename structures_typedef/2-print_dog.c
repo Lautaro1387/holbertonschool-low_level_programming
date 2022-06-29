72e023a554f250f6343ac4e636af5432da35b9eb
Lautaro1387 <5134@holbertonschool.com>
2022-06-29 14:30:04 -0700

    Task 0

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,6 +6,9 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+	}
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)\n");
@@ -22,8 +25,5 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("Owner: %s\n", d->owner);
-	}
-	if (d == NULL)
-	{
 	}
 }
