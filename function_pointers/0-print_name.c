ef94a5acf482655acdf3ef576ecdcd56ee73e38e
Lautaro1387 <5134@holbertonschool.com>
2022-06-30 05:54:49 -0700

    Task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - Function.
+ * @name: Pointer element.
+ * @f: Pointer function.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+
+}
