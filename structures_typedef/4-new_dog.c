16e6dee5ae3c9ee4061a5aeadc01c3a17d0e1e5d
Lautaro1387 <5134@holbertonschool.com>
2022-06-29 18:21:24 -0700

    Task 0

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 #include <string.h>
 /**
  * _str - Function.
@@ -11,16 +12,12 @@ char *_str(char *str)
 	char *dog;
 
 	if (!str)
-	{
 		return (NULL);
-	}
 	for (len = 0; str[len] != 0;)
 		len++;
-	dog = (char *)malloc(sizeof(char) * len + 1);
+	dog = (char *)malloc((sizeof(char) * len + 1));
 	if (!dog)
-	{
 		return (NULL);
-	}
 	for (a = 0; a <= len; len++)
 	{
 		dog[a] = str[a];
@@ -40,9 +37,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 	if (!d)
-	{
 		return (NULL);
-	}
 	d->name = _str(name);
 	if (!d->name)
 	{
