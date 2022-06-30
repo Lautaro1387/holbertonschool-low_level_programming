a7a4ea001a732009e32333cbf65be97061477983
Lautaro1387 <5134@holbertonschool.com>
2022-06-30 06:56:05 -0700

    Task 0

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -4,21 +4,23 @@
  * @array: Pointer element.
  * @size: Element.
  * @cmp: Function pointer.
+ * Return: size2.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int size2;
 
+	if (!cmp || !array)
+        {
+                return (-1);
+        }
+        if (size <= 0)
+        {
+                return (-1);
+        }
 	if (cmp && array)
 	for (size2 = 0; size2 < size; size2++)
-	(*cmp)(array[size2]);
-	if(!cmp)
-	{
-		return (-1);
-	}
-	if (size <= 0)
-	{
-		return (-1);
-	}
+		(*cmp)(array[size2] != 0);
+
 	return (size2);
 }
