dada4cf11e878ab95ebc252fa337f6a46b627620
Lautaro1387 <5134@holbertonschool.com>
2022-06-30 08:20:17 -0700

    Task 0

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,18 +8,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int size2;
+	int size2, x;
 
         if (size <= 0)
         {
                 return (-1);
         }
 	if (cmp && array)
+	{
 	for (size2 = 0; size2 < size; size2++)
-		(*cmp)(array[size2] != 0);
-	if (cmp(array[size2]))
+		x = cmp(array[size2]);
+	if (x != 0)
 	{
 		return (size2);
+	}
 	}
 	return (-1);
 }
