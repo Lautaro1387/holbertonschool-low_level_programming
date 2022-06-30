61d2b0d385322e32912b0d50294aa9725900234d
Lautaro1387 <5134@holbertonschool.com>
2022-06-29 17:30:50 -0700

    Task 0

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,2 +1,59 @@
 #include "dog.h"
+#include <string.h>
+/**
+ * _str - Function.
+ * @str: Pointer element.
+ * Return: dog.
+ */
+char *_str(char *str)
+{
+	unsigned int a, len;
+	char *dog;
 
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (len = 0; str[len] != 0;)
+		len++;
+	dog = (char *)malloc(sizeof(char) * len + 1);
+	if (!dog)
+	{
+		return (NULL);
+	}
+	for (a = 0; a <= len; len++)
+	{
+		dog[a] = str[a];
+	}
+	return (dog);
+}
+/**
+ * new_dog - New structure.
+ * @name: Pointer element.
+ * @age: Element.
+ * @owner: Pointer element.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(_str));
+	if (!d)
+	{
+		return (NULL);
+	}
+	d->name = _str(name);
+	if (!d->name)
+	{
+	free(d);
+	return (NULL);
+	}
+	d->owner = _str(owner);
+	if (!d->owner)
+	{
+	free(d);
+	return (NULL);
+	}
+	d->age = _str(age);
+	return (d);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG
 #define DOG
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - Structure.
  * @name: name.
