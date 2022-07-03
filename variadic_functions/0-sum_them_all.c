6809355ab61dff1adcfc5997a27fbfb1d49dfdc6
Lautaro1387 <5134@holbertonschool.com>
2022-07-02 20:24:43 -0700

    Task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,17 +6,25 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i, p = 0;
+	int a;
 	va_list sum;
-	int i;
 
 	if (n)
 	{
-		va_start(sum, n);
-		for (i = n; i >= 0; i = va_arg(sum, int))
-		return (n);
+	va_start(sum, n);
+	
+		for (i = 0; i < n; i++)
+		{
+		a = va_arg(sum, int);
+		p += a;
+		}
 		va_end(sum);
+		return (p);
 	}
 	if (!n)
+	{
 	return (0);
+	}
 	return (0);
 }
