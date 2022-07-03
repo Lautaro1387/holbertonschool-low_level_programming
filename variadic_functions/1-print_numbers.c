93a60be3cf2258eaddd9eb7ea0f27ca1f102737d
Lautaro1387 <5134@holbertonschool.com>
2022-07-02 22:28:53 -0700

    Task 0

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,5 +3,27 @@
  * print_numbers - Function.
  * @separator: Pointer element.
  * @n: Element.
+ * Return: void.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+	char a, p = 0;
+
+	if (separator)
+	{
+		va_start(num, n);
+		for (i = 0; i < n; i++)
+		{
+		a = va_arg(num, int);
+		p += a;
+		printf("%d\n", p);
+		}
+		va_end(num);
+	}
+	if (!separator)
+	{
+	}
+	printf("\n");
+}
