b88610556162e64f77264913480f262d720c232f
Lautaro1387 <5134@holbertonschool.com>
2022-07-03 13:32:10 -0700

    Task 0

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,3 +4,24 @@
  * @format: Element.
  */
 void print_all(const char * const format, ...)
+{
+	va_list ran;
+	char c = 0;
+	int i = 0;
+	float f = 0.0f;
+	char *s = 0;
+
+	va_start(ran, format);
+	while (c < format && i < format && f < format)
+	s = va_arg(ran, char *);
+	if (s == NULL)
+	{
+	printf("%s", s);
+	}
+	if (s == NULL)
+	{
+	printf("(nil)");
+	}
+	printf("\n");
+	va_end(ran);
+}
