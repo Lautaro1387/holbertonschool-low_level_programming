e6613b2607c138bca80d65ab4642e9a37683f7d8
Lautaro1387 <5134@holbertonschool.com>
2022-07-04 08:32:50 -0700

    Task 0

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,27 +1,86 @@
 #include "variadic_functions.h"
 /**
+* print_int - Function
+* @list: List
+* Return: Void
+*/
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_char - Function
+ * @list: List
+ * Return: Void
+*/
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+* print_float - Function.
+* @list: List
+* Return: Void
+*/
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+* print_string - Function.
+* @list: List.
+* Return: Void
+*/
+void print_string(va_list list)
+{
+	char *string;
+
+	string = va_arg(list, char *);
+	if (string == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s", string);
+}
+
+/**
  * print_all - Function.
- * @format: Element.
- */
+ * @format: Const element.
+ * Return: Void.
+*/
 void print_all(const char * const format, ...)
 {
-	va_list ran;
-	char c = 0;
-	int i = 0;
-	float f = 0.0f;
-	char *s = 0;
+	int i, a;
+	va_list list;
+	char *z = "";
 
-	va_start(ran, format);
-	while (c < format && i < format && f < format)
-	s = va_arg(ran, char *);
-	if (s == NULL)
+	alls prints[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	va_start(list, format);
+	while (format != NULL)
 	{
-	printf("%s", s);
+		a = 0;
+		while (prints[a].letters != '\0')
+		{
+			if (prints[a].letters == format[i])
+			{
+			printf("%s", z);
+			prints[a].f(list);
+			z = ", ";
+			}
+		a++;
+		}
+	i++;
 	}
-	if (s == NULL)
-	{
-	printf("(nil)");
-	}
+	va_end(list);
 	printf("\n");
-	va_end(ran);
 }
