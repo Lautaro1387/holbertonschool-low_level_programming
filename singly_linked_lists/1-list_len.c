60803772d239c24555d3383f64adaf17820a5c7a
Lautaro1387 <5134@holbertonschool.com>
2022-07-13 11:32:00 -0700

    Task 0

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - Function of type size_t
+ * @h: Const.
+ * Return: size_t.
+ */
+size_t list_len(const list_t *h)
+{
+size_t size = 0;
+
+	while (h != NULL)
+	{
+		size++;
+		if (h->str)
+		print_list(h->str);
+	}
+	h = h->next;
+	return (size);
+}
