a7296ea5fefdd941b1a04cd337abdc3866d53e26
Lautaro1387 <5134@holbertonschool.com>
2022-07-15 06:23:00 -0700

    Task 0

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Function.
+ * @head: Pointer element.
+ * @n: Element of type int.
+ * Return: listint_t.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_list;
+        listint_t *new_new;
+
+        new_list = malloc(sizeof(listint_t));
+
+        if (!new_list)
+        {
+                return (NULL);
+        }
+        new_list->n = n;
+        new_list->next = NULL;
+        if (*head == NULL)
+        {
+                *head = new_list;
+        }
+        else
+	{
+			new_new = *head;
+			while (new_new->next)
+			new_new = new_new->next;
+			new_new->next = new_list;
+        }
+        return (*head);
+}
