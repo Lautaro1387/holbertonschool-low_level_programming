d32e5fec66c3e9c484824eafe8852f8bf850ed95
Lautaro1387 <5134@holbertonschool.com>
2022-07-15 09:42:55 -0700

    Task 0

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,2 +1,21 @@
 #include "lists.h"
+/**
+ * pop_listint - Function
+ * @head: Pointer element.
+ * Return: int.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *head2;
+	int delete;
 
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	head2 = *head;
+	delete = (*head)->n;
+	*head = (*head)->next;
+	free(head2);
+	return (delete);
+}
