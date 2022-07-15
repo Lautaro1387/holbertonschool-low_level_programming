02f9e8935731422d438fc1c5da17bb1c6a3a9d2b
Lautaro1387 <5134@holbertonschool.com>
2022-07-15 08:05:31 -0700

    Task 0

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - Function.
+ * @head: Pointer element.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *free_list;
+
+	while (head)
+	{
+		free_list = *head;
+		*head = free_list->next;
+		free(free_list);
+	}
+	if (head == NULL)
+	{
+	printf("(nil)\n");
+	}
+}
