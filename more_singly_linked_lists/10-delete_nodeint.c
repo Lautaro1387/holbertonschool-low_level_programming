ee9ed492b148562289277db167a2434a9c3a4b57
Lautaro1387 <5134@holbertonschool.com>
2022-07-15 11:27:08 -0700

    Task 0

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - Function.
+ * @head: Pointer element.
+ * @index: Element of type int.
+ * Return: int.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	int delete = 0;
+
+	return (*head);
+}
