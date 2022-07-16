dc03ca9daa7142102c4b9efe51928b39cb73cbab
Lautaro1387 <5134@holbertonschool.com>
2022-07-15 17:23:50 -0700

    Task 0

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -7,7 +7,36 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	int delete = 0;
+	unsigned int num = 0;
+	listint_t *nodo, *sig;
 
-	return (*head);
+	if (head && *head)
+	{
+		nodo = *head;
+		if (index > 0)
+		{
+			if (nodo->next == NULL)
+				return (-1);
+			while (nodo)
+			{
+				if (index - 1 == num)
+				{
+					sig = nodo->next->next;
+					free(nodo->next);
+					nodo->next = sig;
+					return (1);
+				}
+				num++;
+				nodo = nodo->next;
+			}
+		}
+		else
+		{
+			*head = nodo->next;
+			free(nodo);
+			return (1);
+		}
+		return (1);
+	}
+	return (-1);
 }
