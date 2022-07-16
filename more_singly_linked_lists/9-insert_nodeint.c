c3f3f525b25591158694b8c6769b57eb60158ad3
Lautaro1387 <5134@holbertonschool.com>
2022-07-15 17:15:17 -0700

    Task 0

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node;
+	listint_t *node, *new_node;
+	unsigned int pos = 0;
 
-	return (*head);
+	node = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = (*head);
+		*head = new_node;
+		return (*head);
+	}
+	while (pos < idx - 1 && node)
+	{
+		node = node->next;
+		pos++;
+	}
+	if (pos >= idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = node->next;
+	node->next = new_node;
+	return (new_node);
 }
