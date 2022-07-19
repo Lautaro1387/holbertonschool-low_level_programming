a4c566e7c4d5c831cec1cdd1107571e03855a2f2
Lautaro1387 <5134@holbertonschool.com>
2022-07-19 12:25:16 -0700

    Task 0

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - Function.
+ * @n: Element of type unsigned long int.
+ * @m: Element of type unsigned long int.
+ * Return: unsigned int.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num = 0, count = 0;
+
+	num = n ^ m;
+	while (num)
+	{
+		if (num & 1)
+		{
+			count++;
+		}
+		num >>=1;
+	}
+	return (count);
+}
