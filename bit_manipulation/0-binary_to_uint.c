dc53f4d567c5b56e720584ab79d7eeb541df019d
Lautaro1387 <5134@holbertonschool.com>
2022-07-19 07:12:08 -0700

    Task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - Function.
+ * @b: Pointer element of type char.
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 0;
+	int i;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (i = 0; b[i]; i++)
+	{
+	if (b[i] < '0' || b[i] > '1')
+	{
+		return (0);
+		a = a + 0 + (b[i] - 0);
+	}
+	}
+	return (i);
+}
