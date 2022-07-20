89f25700852a092c53550741545c3d19010f2339
Lautaro1387 <5134@holbertonschool.com>
2022-07-20 09:59:56 -0700

    Task 0

--- file_io/main.h
+++ file_io/main.h
@@ -2,6 +2,7 @@
 #define FILE I/O
 #include <stdio.h>
 #include <stdlib.h>
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
