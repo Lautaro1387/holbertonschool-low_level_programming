a2cd00678c855ec7b94fd750fc3b26aca7a19487
Lautaro1387 <5134@holbertonschool.com>
2022-07-20 17:10:44 -0700

    Task 0

--- file_io/main.h
+++ file_io/main.h
@@ -1,7 +1,12 @@
-#ifndef FILE
-#define FILE
+#ifndef FILE_H
+#define FILE_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
