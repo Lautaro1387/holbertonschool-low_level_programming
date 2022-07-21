56363c8ef469839c42fc676884d23b7bb14a15ba
Lautaro1387 <5134@holbertonschool.com>
2022-07-21 09:45:28 -0700

    Task 0

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
-		return (0);
+		return (NULL);
 	}
 	if (new_node != NULL)
 	{
---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - Function.
+ * @head: Pointer element.
+ * @n: Element of type int.
+ * Return: dlistint_t.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+	dlistint_t *new_new = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (new_new != NULL)
+		{
+			if (new_new->next == NULL)
+			{
+				new_new->n = n;
+				new_new->next = new_node;
+				new_new->next->prev = new_new;
+				new_new = new_node;
+			}
+			new_new = new_new->next;
+		}
+	}
+	return (*head);
+}
