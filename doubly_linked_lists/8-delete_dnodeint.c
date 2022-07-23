528ef1d8aab73dfe01570afee67e31a3f7995e4e
Lautaro1387 <5134@holbertonschool.com>
2022-07-22 19:12:49 -0700

    Task 0

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * dlistint_len - Function.
- * @h: Pointer element.
- * Return: size_t.
+ * dlistint_len - Returns the number of nodes in a doubly linked list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
@@ -10,10 +10,50 @@ size_t dlistint_len(const dlistint_t *h)
 
 	if (!h)
 		return (0);
+
 	while (h)
 	{
 		nodes++;
 		h = h->next;
 	}
+
 	return (nodes);
+}
+
+/**
+ * delete_dnodeint_at_index - Function.
+ * @head: double pointer to the list.
+ * @index: index of the node to delete.
+ * Return: 1 on success.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
+
+	if (*head == NULL || dlistint_len(temp) < index + 1)
+		return (-1);
+
+	if (!index)
+	{
+		(*head) = temp->next;
+		if (temp->next)
+			temp->next->prev = NULL;
+		temp->next = NULL;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+	free(temp);
+
+	return (1);
 }
