23e00e07c66c1e727b211e25acfa9f999445bdb1
Lautaro1387 <5134@holbertonschool.com>
2022-08-21 17:54:27 -0700

    Created hash tables and add djb2 algoritm

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *hash = NULL;
+
+	hash = malloc(sizeof(hash_table_t));
+	if (!hash)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	hash->size = size;
+	hash->array = malloc(size * sizeof(hash_table_t ));
+	if (!hash->array)
+	{
+		free(hash);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		hash->array[i] = NULL;
+		i++;
+	}
+	return (hash);
+}
